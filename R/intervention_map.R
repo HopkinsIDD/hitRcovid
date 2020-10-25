c2221741eb15514be123a291952be064ab4844c8
Hanmeng-Xu <72403355+Hanmeng-Xu@users.noreply.github.com>
2020-10-25 12:46:19 -0400

    Finalized intervention_map function

--- R/intervention_map.R
+++ R/intervention_map.R
@@ -1,66 +1,54 @@
 
 ## UNDER DEVELOPMENT ##
 
-
-## SVL - Todo:
-## 1. Remove dplyr and rewrite in base R (so no pipes, etc.) I know this is annoying but when you use
-## a package, the fact that dplyr does not put column names in quotes causes issues.
-## 2. Add documentation (see structure below and the hit_filter() function)
-## 3. Add examples (see structure below and the hit_filter() function)
-## 4. If you want to learn how to write formal tests we can set up a call and I can show you how
-## and you can add the tests.
-
-
-## SVL - documentation template
-
-#' Sentence title
+#' Mapping of interventions 
 #' 
-#' Paragraph description (comes before arguments in output)
+#' This function maps the interventions at both national and administrative level at a specific time.
+#' The HIT-COVID dataset is loaded with \link{hit_pull}, and data of one specific intervention
+#' is pulled using \link{hit_filter}. 
+#' The function aims to draw a world map depicting the latest interventions before this \code{time_point}.
+#' If no value is entered for \code{time_point}, it will return a map of most recent interventions.
+#' Both national level and administrative level data are mapped on the same plot. 
+#' National level data are represented by the color of country territories, and administrative level data are 
+#' represented by dots, both types of data are coded in red (Strongly implemented),
+#' yellow (partially implemented) or green (implementation suspended). The strongest implementation 
+#' intensity is used for mapping if for one territory there are multiple entries in the same intervention group on the same day. 
 #' 
-#' Multiple paragraphs of details as necessary to explain arguments and functioning in more detail
-#' (comes after arguments in output)
-#' 
-#' @param argument_name description of the argument
+#' @param hit_data the full HIT-COVID database pulled from GitHub, pulled using \link{hit_pull}
+#' @param time_point character string indicating the desired mapping date (default is Sys.Date())
+#' @param intervention_group vector of intervention group to filter the data to 
+#' (see \link{intervention_lookup} column "intervention_group" or run \link{list_interventions} for options)
+#' @param date.format character string indicating the desired format of date. (default is "%m/%d/%Y")
 #' 
 #' @return 
-#' What the function returns (here a plot)
+#' A world map depicting interventions of countries and administrative divisions by a specific date.
 #' 
 #' @examples 
-#' # Add examples of usage showing what different options do
+#' # Mapping of most recent interventions in "quarantine and isolation" domain
+#' intervention_map(hit_data, intervention_group = "quar_iso")
+#' 
+#' # Mapping of latest "quarantine and isolation" interventions before 5/24/2020
+#' intervention_map(hit_data, intervention_group = "quar_iso", time_point = "5/24/2020")
 #' 
 #' @seealso \link{hit_filter}
 #' 
 #' @references 
-#' Add any references if relevant
+#' https://gadm.org/download_world.html 
+#' (to get the longitude and latitude of administrative divisions)
+#' HX: Also based on Sarah's gadm_map dataset.
 #' 
 #' @importFrom rlang .data
 #' 
 #' @export
 
 
-
-
-## SVL - I added maps to the package dependencies, but it is better to avoid the tidyverse
-## SVL - I also removed maps because it appears you are not using it. If you need to use other packages,
-## use package:: to refer to it. Then run use_package("packagename") to add it to the dependencies.
-
-
-## SVL - remove these comments at some point
-
-### int_map is the function of generating map for each intervention at a given time
-###################################################################################
-### currently just making static map, may add hover feature if needed 
-
-## SVL - changed name to be consistent
-
 intervention_map <- function(hit_data,
                     time_point = Sys.Date(), 
-                    intervention_group = NULL, ## SVL - changed to match name in other functions
+                    intervention_group = NULL, 
                     date.format = "%m/%d/%Y") {
   
   # Error handling-----------------------------------------------------------------
   
-  ## SVL - Changed warning() to stop() to give an error
   
   # Determine if the intervention_group entered is valid, if not, return warning
   if(!intervention_group %in% intervention_lookup$intervention_group){
@@ -70,19 +58,15 @@ intervention_map <- function(hit_data,
   # Determine if the time_point entered is valid, if not, return warning
   time_point <- as.Date(time_point, date.format)  
   if(is.na(time_point)) {
-    stop('Time point entered here is not valid, please enter valid time in "%m/%d/%y" format.')
+    stop('Time point entered here is not valid, please enter a valid date in right format.')
   } else if(time_point < as.Date("1/1/2020", date.format) |
             time_point > Sys.Date()) {
-    stop("Time point entered here is not valid, please enter valid time between 1/1/2020 and present.")
+    stop("Time point entered here is not valid, please enter valid date between 1/1/2020 and present.")
   } 
   
-  ## SVL - I removed pulling the data from inside the function to make it faster - assume someone
-  ## has loaded it before using it and add as an argument.
-  
-  # Prepare data for visualization ----------------------------------------------------------------
   
   # update country names
-  hit_data$country_name=as.character(hit_data$country_name)
+  hit_data$country_name = as.character(hit_data$country_name)
   hit_data[which(hit_data$country_name=='Hong Kong'),]$country_name ='China'
   hit_data[which(hit_data$country_name=='Korea Republic of'),]$country_name='South Korea'
   hit_data[which(hit_data$country_name=='Bolivia Plurinational State of'),]$country_name='Bolivia'
@@ -104,7 +88,7 @@ intervention_map <- function(hit_data,
   hit_data[which(hit_data$country_name=='Congo'),]$country_name='Republic of Congo'
   
   # subset hit_data by intervention_group
-  hit_data <- hit_filter(hit_data, intervention_group = intervention_group, usa_county_data = FALSE)
+  hit_data <- hit_filter(hit_data, intervention_group = intervention_group, usa_county_data = "exclude")
   
   # Prepare the dates
   hit_data$date_of_update <- as.Date(as.character(hit_data$date_of_update, format = "%y-%m-%d"))
@@ -115,67 +99,92 @@ intervention_map <- function(hit_data,
                                              "Partially Implemented",
                                              "Strongly Implemented"))
   
-  ## SVL - I think if we are plotting national data, we should just use national data (not admin1)
-  ## Thoughts? If so you could add include_admin1 = FALSE to the hit_filter() statement
+  # SVL - I think if we are plotting national data, we should just use national data (not admin1)
+  # Thoughts? If so you could add include_admin1 = FALSE to the hit_filter() statement
+  # HX: I pulled them all first in one dataframe to get "most recent strongest record" for both
+  #     and then subset separately to make country or admin map, hope this makes sense!
   
   # Get latest & strongest implementation status at country or admin1 level
-  hit_data$admin1 <- as.character(hit_data$admin1)
-  recent_data <- hit_data %>%
-    filter(date_of_update <= time_point) %>%
-    group_by(country_name, admin1) %>%
-    filter(date_of_update == max(date_of_update)) %>%
-    group_by(country_name, admin1, date_of_update) %>%
-    summarize(recent_status = max(status_simp))
+  recent_data <- dplyr::filter(hit_data, date_of_update <= time_point)
+  recent_data <- dplyr::group_by(recent_data, country, country_name, admin1)
+  recent_data <- dplyr::filter(recent_data, date_of_update == max(date_of_update))
+  recent_data <- dplyr::group_by(recent_data, country, country_name, admin1, date_of_update)
+  recent_data <- dplyr::summarise(recent_data, recent_status = max(status_simp))
   
-  
-  # Mapping of country level data -----------------------------------------------------------------
-  
+  # country level data prep -----------------------------------------------------
   # load the world map (remove Antarctica) and merge with country level data
   world <- ggplot2::map_data("world")
   world <- world[which(!world$region=='Antarctica'),]  
-  country_map <- recent_data %>% filter(is.na(admin1))
-  country_map <- left_join(world, country_map, world, by = c("region" = "country_name"))
+  country_map <- dplyr::filter(recent_data, is.na(admin1))
+  country_map <- dplyr::left_join(world, country_map, world, by = c("region" = "country_name"))
   
+  #' HX: 
+  #' I used the dataset "gadm_admin1.RData" in covid19-interventions package created by Sarah for the weekly update report,
+  #' and calculated one coordinate for each admin polygon, (as we want dots for admin not polygon) 
+  #' I stored this in a file (containing ISO, admin_name, long and lat, etc.), 
+  #' (and currently put this file in the same folder with geo_lookup)
+
+  # admin level data prep -------------------------------------------------------
+  admin_location <- hitRcovid::admin_location
+  admin_location <- dplyr::select(dplyr::left_join(hitRcovid::geo_lookup, admin_location,
+                                              by = c("admin1_name" = "admin_name", "country" = "ISO")),
+                            country, country_name, admin1_name, long, lat, admin1, continent)
+
+   # merge and delete duplicate (some country are counted in >1 continents in geo_lookup)
+  admin_map <- dplyr::filter(recent_data, !is.na(admin1))
+  admin_map <- dplyr::left_join(admin_map, admin_location, by = c("admin1", "country"))
+  admin_map <- dplyr::summarise(dplyr::group_by(admin_map, 
+                         country, country_name.x, admin1, admin1_name, date_of_update, recent_status, long, lat),
+                         continent = max(continent))
+   
+  # HX: some coordinates (lat and long) of admin1 are missing from gadm_map database,
+  #     currently just delete them when mapping, to avoid warning message
+  admin_map <- admin_map[which(!is.na(admin_map$long)), ]
+
+  # Setting legends -----------------------------------------------------------------
   # create country level data legend
   country_map$recent_status <- as.character(country_map$recent_status)
   country_map[which(is.na(country_map$recent_status)), ]$recent_status <- "No data"
   country_map$recent_status <- ordered(country_map$recent_status, 
-                                       levels = c("Strongly Implemented", 
-                                                  "Partially Implemented", 
-                                                  "Implementation Suspended", 
-                                                  "No data"))
-  
-  # set the legend colors (considering some recent_status may have 0 record)
-  legend <- as.data.frame(table(country_map$recent_status))
-  legend$country_colors = c("lightpink2", "khaki2", "aquamarine2", "gray93")
-  country_mapping_colors <- legend[which(legend$Freq > 0), ]$country_colors
-  
-  ## SVL - When writing a package you don't want to save things because that changes the users device
-  ## instead print things then users can save.
-  
-  # draw country level intervention map with ggplot
-   p <- ggplot2::ggplot(data = country_map, 
-         ggplot2::aes(x = .data$long, y = .data$lat, group= .data$group, fill=.data$recent_status)) +
-    ggplot2::scale_fill_manual(values = country_mapping_colors) +
-    ggplot2::geom_polygon(colour='black')+
-    ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white")
-          ,plot.background = ggplot2::element_rect(fill = "white")
-          ,panel.grid = ggplot2::element_blank()
-          ,axis.text = ggplot2::element_blank()
-          ,axis.title = ggplot2::element_blank())+
-    ggplot2::labs(fill = "Intervention intensity")+
-    ggplot2::theme(legend.title.align = 0.5)+
-    ggplot2::theme(legend.text = ggplot2::element_text(size=12),
-          legend.title = ggplot2::element_text(size=14))
-  
-  # Mapping of admin1 level data ------------------------------------------------------------------
- 
-
-  
-  
-   print(p)
+                                        levels = c("Strongly Implemented", 
+                                                   "Partially Implemented", 
+                                                   "Implementation Suspended", 
+                                                   "No data"))
+   
+  # set color palette and legend for country level data (considering some recent_status may have 0 record)
+  country_legend <- as.data.frame(table(country_map$recent_status))
+  country_legend$country_colors = c("lightpink2", "khaki2", "aquamarine2", "gray93")
+  country_mapping_colors <- country_legend[which(country_legend$Freq > 0), ]$country_colors
+   
+   
+  # set color palette and legend for admin data points
+  admin_legend <- as.data.frame(table(admin_map$recent_status))
+  admin_legend$admin_colors = c("green4", "orange2", "red4")
+  admin_mapping_colors <- admin_legend[which(admin_legend$Freq > 0), ]$admin_colors
+   
+  # map data of both level ---------------------------------------------------------
+  p <- ggplot2::ggplot() +
+     ggplot2::geom_polygon(data = country_map, aes(x = .data$long, y = .data$lat, group = .data$group, fill = .data$recent_status), 
+                  color = "black", size = 0.2, alpha = 0.7) +
+     ggplot2::scale_fill_manual(values = country_mapping_colors) +
+     ggplot2::geom_point(data = admin_map, aes(x = .data$long, y = .data$lat, col = .data$recent_status),
+                alpha = 0.5) +
+     ggplot2::scale_color_manual(values = admin_mapping_colors) +
+     ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white")
+                    ,plot.background = ggplot2::element_rect(fill = "white")
+                    ,panel.grid = ggplot2::element_blank()
+                    ,axis.text = ggplot2::element_blank()
+                    ,axis.title = ggplot2::element_blank()
+                    ,axis.ticks = element_blank())+
+     ggplot2::labs(fill = "Intervention intensity")+
+     ggplot2::guides(color = FALSE) +
+     ggplot2::theme(legend.title.align = 0.5)+
+     ggplot2::theme(legend.text = ggplot2::element_text(size=8),
+                    legend.title = ggplot2::element_text(size=10)) +
+     ggplot2::coord_fixed(ratio = 1.3)
+   
+  print(p)
 }
 
-## SVL - Removed examples and moved to "Package_Testing.R"
 
 
